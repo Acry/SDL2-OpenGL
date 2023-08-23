472788b84fe7954df92a0b6675b02065ff2d916e
Carsten Holtkamp <caholtk@gmail.com>
2023-08-23 17:39:19 +0200

    fix default shader

--- src/default_shader.h
+++ src/default_shader.h
@@ -1,12 +1,12 @@
+// code from 2.vert as array of chars
 static const char vertex_shader[] =
 "#version 110\n"
-"in vec4 fragColor;"
-"varying out vec2 fragCoord;"
+"varying vec2 fragCoord;\n"
 "void main(){gl_Position = gl_Vertex;fragCoord = gl_Position.xy;}";
 
+// code from 5.frag as array of chars
 static const char fragment_shader[] =
 "#version 110\n"
-"in vec2  fragCoord;"
-"uniform float fTime;"
-"varying out vec4  fragColor;"
-"void main(){vec2  uv  = vec2( (fragCoord.xy - -1.0) / 2.0 );vec3  col = 0.5 + 0.5 * cos( fTime + uv.xyx + vec3(0,2,4) );fragColor = vec4(col,1.0);}";
+"varying vec2  fragCoord;\n"
+"uniform float fTime;\n"
+"void main(){vec2  uv  = vec2( (fragCoord.xy + 1.0) / 2.0 );vec3  col = 0.5 + 0.5 * cos( fTime + uv.xyx + vec3(0,2,4) );gl_FragColor = vec4(col,1.0);}";
